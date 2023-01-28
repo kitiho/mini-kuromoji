c1a63d87faaa3a796582de45afa08240b8c028d0
enoshima7 <duanjipeng0328@gmail.com>
2023-01-28 14:59:31 +0800

    feat: add error handle

--- example/load-node.ts
+++ example/load-node.ts
@@ -2,8 +2,9 @@ import kuromoji from '../dist/index.js'
 
 const DIC_DIR = 'dict/'
 
-// eslint-disable-next-line n/handle-callback-err
 kuromoji.builder({ dicPath: DIC_DIR }).build((error, tokenizer) => {
+  if (error)
+    console.log(error)
   const path = tokenizer.tokenize('俺の家族は幸せだそうです。')
   console.log(path)
 })
